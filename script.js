58d2c3ddd21df4189055f1418d3e7f81a325c57d
Prathmesh Ingole <91962775+IORD1@users.noreply.github.com>
2023-03-16 09:06:25 +0530

    updated added xml

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ function func() {
   var name = document.getElementById("namein").value;
   var emails = document.getElementById("emailin").value;
   var phnum = document.getElementById("unumin").value;
-  
+  var prn = document.getElementById("prn").value;
+
   if(name == ""){
     alert("Please Enter your name")
     flag = 0;
@@ -18,6 +19,14 @@ function func() {
     alert("Please Enter you Phone number")
     flag = 0;
   }
+
+
+  if(/\d/.test(name)){
+    alert("Enter Valid Name")
+    flag = 0;
+  };
+
+  
   if(Number.isInteger(phnum) ){
     alert("Phone Number should a Number")
     flag = 0;
@@ -32,16 +41,34 @@ function func() {
     flag = 0;
   }
 
-  var validRegexforEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  var validRegexforEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
   if(!emails.match(validRegexforEmail)){
     alert("Please enter valid email address");
     flag = 0;
   }
 
+
+  if(isNaN(prn)){
+    alert("Please Enter Number for PNR");
+    flag = 0;
+  }
+  if(prn.length !== 10){
+    alert("Plese Enter Valid PNR");
+      flag = 0;
+  }
   
 
   if(flag){
-    alert("Successfully Registered for Newsletter")
+    alert("Successfully Added Subscription");
+    location.reload();
+    document.getElementById("namein").value = "";
+    document.getElementById("emailin").value = "";
+    document.getElementById("unumin").value = "";
+    document.getElementById("prn").value = "";
+
+  }else{
+    return;
   }
   
 }
